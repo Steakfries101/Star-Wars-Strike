0fdd9122d88bbbef4b3578bfb8522d7d2eeb4a6e
Evan Shaw <47705658+Steakfries101@users.noreply.github.com>
2024-11-05 11:32:22 -0800

    Commit project after moving

---
+++ README.md
@@ -0,0 +1,17 @@
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/4mwGR0DK)
+[![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-7f7980b617ed060a017424585567c406b6ee15c891e84e1186181d67ecf80aa0.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=12483480)
+# Module 3 
+Module 3 for DGL 213 Applied JavaScript.
+
+## Contents
+The contents of this repository includes some of the code built as part of the first module of the *The Coding Train* series [Working with Data and APIs in JavaScipt](https://www.youtube.com/playlist?list=PLRqwX-V7Uu6YxDKpFzf_2D84p0cyk4T7X). Note that the license attributes copyright to *The Coding Train* and must be maintained (at least in part) if any part of the code in this repository is used in another project.
+
+## package.json
+A package.json for this project has already been created. Note that it includes *The Coding Train* as the code author, since at present all code in the repository was developed by *The Coding Train*. You may change this after you have developed your own project, depending on the degree to which you have developed your own code.
+
+Note that ESLint has also already been installed as a dev dependency.
+
+## Usage
+To use this repository clone it then navigate to the cloned folder, open a terminal window and type `npm install` to activate the project and install any dependencies listed in the `package.json` file.
+
+You can preview the app locally using `npx live-server docs`
---
+++ script.js
@@ -0,0 +1,88 @@
+const bcPlaceNameApiUrl =
+  "http://geogratis.gc.ca/services/geolocation/en/locate?q=,%20BC";
+
+const laserIcon = L.icon({
+  iconUrl: "images/laser.png",
+  iconSize: [26, 26],
+  iconAnchor: [13, 13],
+});
+document.getElementById("getPlanetInfo").addEventListener("click", emptyPlanCheck);
+document.getElementById("getCharacterInfo").addEventListener("click", emptyCharCheck);
+async function getCoordinates() {
+  try {
+    const response = await fetch(bcPlaceNameApiUrl);
+    const data = await response.json();
+    const coordinatesArray = [];
+    data.forEach((item) => {
+      const names = item.title;
+      const latitude = item.geometry.coordinates[1];
+      const longitude = item.geometry.coordinates[0];
+
+      coordinatesArray.push({ latitude, longitude, names });
+    });
+    coordinatesArray.forEach((item) => {
+      L.marker([item.latitude, item.longitude], { icon: laserIcon })
+        .addTo(myMap)
+        .bindPopup(item.names, item.population);
+    });
+  } catch (error) {
+    console.error("ERROR:", error);
+  }
+}
+
+async function displayCharacter(characterName) {
+  const response = await fetch(`https://swapi.dev/api/people/?search=${characterName}`);
+  const data = await response.json();
+  if (data.count === 0) {
+    alert(`No character found with the name ${characterName}`);
+  } else {
+    const character = data.results[0];
+    const characterInfo = `
+      <li>Name: ${character.name}</li>
+      <li>Hair Color: ${character.hair_color}</li>
+      <li>Eye Color: ${character.eye_color}</li>
+      <li>Birth Year: ${character.birth_year}</li>
+      <li>Gender: ${character.gender}</li>
+      <li>Height: ${character.height} cm</li>
+      <li>Mass: ${character.mass} kg</li>
+      
+    `;
+
+    document.getElementById("characterInfo").innerHTML = characterInfo;
+  }
+}
+function emptyCharCheck() {
+  const characterName = document.getElementById("characterInput").value;
+  if (characterName === "") {
+    alert("Please enter a characters name!");
+  } else {
+    displayCharacter(characterName);
+  }
+}
+
+async function displayPlanet(planetName) {
+  const response = await fetch(`https://swapi.dev/api/planets/?search=${planetName}`);
+  const data = await response.json();
+  if (data.count === 0) {
+    alert(`No planet found with the name ${planetName}`);
+  } else {
+    const planet = data.results[0];
+    const planetInfo = `
+    <li>Name: ${planet.name}</li>
+    <li>Climate: ${planet.climate}</li>
+    <li>Terrain: ${planet.terrain}</li>
+    <li>Population: ${planet.population}</li>
+    <li>Gravity: ${planet.gravity}</li>
+  `;
+    document.getElementById("planetInfo").innerHTML = planetInfo;
+  }
+}
+
+function emptyPlanCheck() {
+  const planetName = document.getElementById("planetInput").value;
+  if (planetName == "") {
+    alert("Please enter a planet name!");
+  } else {
+    displayPlanet(planetName);
+  }
+}
